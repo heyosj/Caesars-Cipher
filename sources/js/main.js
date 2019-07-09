7241729a15cca8ccf480fc02d5ba60c518f9b402
OJ Sanchez <ojsanch@gmail.com>
2019-07-08 21:03:52 -0500

    updated UI, clear button added

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,12 +1,14 @@
 const submitButton = document.querySelector('.btn');
 const inputField = document.querySelector('.longInput');
 const outputField = document.querySelector('.outputField');
+const clear = document.querySelector('.clear');
 
 submitButton.addEventListener('click', function() {
     input = inputField.value;
     capInput = input.toUpperCase();
     caesarCipher(capInput);
 });
+
 
 // [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
 function caesarCipher(str) {
@@ -20,11 +22,16 @@ function caesarCipher(str) {
         } else {
             solved += str[i];
         }
-    }
-    
-    console.log(solved.toLowerCase());
+    };
     outputField.innerHTML = solved.toLowerCase();
-    return solved;
-}
+    // outputField.appendChild(solved);
+    console.log(solved.toLowerCase());
+};
+
+function clearButton() {
+    inputField.value = '';
+    outputField.value = '';
+
+};
 
 // Lower case letter still need to be accepted
